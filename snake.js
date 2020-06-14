27252eca55ade351b76093b7f5c76d0a38e5ded9
hp <chhabraashish123@gmail.com>
2020-06-15 04:30:44 +0530

    KeyBoard Movement(Action Event Listener) updated

--- snake.js
+++ snake.js
@@ -23,28 +23,61 @@ function init()
       drawsnake:function()
       {
       	 //Evertime erase the previous frame to draw the new frame
-      	 pen.clearRect(0,0,W,H);
          for(var i=0;i<this.cells.length;i++){
          	pen.fillStyle=this.color;
          	pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-2,cs-2);
        }
       },
 
-      updatesnake:function()
-      {
-          this.cells.pop();
-          var headx=this.cells[0].x;
-          var heady=this.cells[0].y;
-          var X=headx+1;
-          var Y=heady;
-          this.cells.unshift({x:X,y:Y});
+           updatesnake:function()
+           {
+      	    //Update snake according to the direction property
+            this.cells.pop();
+            var headX=this.cells[0].x;
+            var headY=this.cells[0].y;
+            var nextX,nextY;
+			if(this.direction=="right"){
+				nextX = headX + 1;
+				nextY = headY;
+			}
+			else if(this.direction=="left"){
+				nextX = headX - 1;
+				nextY = headY;
+			}
+			else if(this.direction=="down"){
+				nextX = headX;
+				nextY = headY + 1;
+			}
+			else{
+				nextX = headX;
+				nextY = headY - 1;
+			}
+
+			this.cells.unshift({x: nextX,y:nextY}); 	
       }
    };
    snake.createsnake();
+   //Add a Event listener on a document object 
+   //e is implicitly passed which contains the meta data or the information about the keyPressed
+   function KeyPressed(e)
+   {
+       if(e.key=="ArrowRight"){
+       	 snake.direction="right";
+       }else if(e.key=="ArrowLeft"){
+       	 snake.direction="left";
+       }else if(e.key=="ArrowDown"){
+       	 snake.direction="down";
+       }else {
+         snake.direction="up"; 
+       }
+       console.log(snake.direction);
+   }
+   document.addEventListener('keydown',KeyPressed) ;
 }
 
 function draw()
 {
+	pen.clearRect(0,0,W,H);
 	snake.drawsnake();
 }
 function update()
