0dc74c891b868ba88d7e37b56197f86f0d271979
hp <chhabraashish123@gmail.com>
2020-06-15 03:36:13 +0530

    Board and snake updated

--- snake.js
+++ snake.js
@@ -6,36 +6,45 @@ function init()
    H=canvas.height=600;
    pen=canvas.getContext('2d');
    game_over=false;
-   rect={
-   	x:20,
-   	y:20,
-   	w:50,
-   	h:40,
-   	speed:10,
-   }  
+   cs=66;
+   snake={
+      init_len: 5,
+      color: "blue",
+      cells: [],
+      direction: "right",
 
+      createsnake:function()
+      {
+      	for(var i=this.init_len;i>0;i--){
+      		this.cells.push({x:i,y:0});
+      	}
+      },
+
+      drawsnake:function()
+      {
+         for(var i=0;i<this.cells.length;i++)
+         	pen.fillRect(this.cells[i].x*cs,this.cells[i].y*cs,cs-2,cs-2);
+      }
+   };
+   snake.createsnake();
 }
 
 function draw()
 {
-   pen.clearRect(0,0,W,H);
-   pen.fillRect(rect.x,rect.y,rect.w,rect.h);
-   pen.fillStyle="red";
+	snake.drawsnake();
 }
 function update()
 { 
-   rect.x+=rect.speed;
-   if(rect.x>W-rect.w||rect.x<0)
-   	  rect.speed *= -1;
+
 }
 function gameloop()
 {
-	if(game_over==true)
-	{
-		clearInterval(f);
-	 	alert("Game Over");
-	 	return;
-	 }
+	// if(game_over==true)
+	// {
+	// 	clearInterval(f);
+	//  	alert("Game Over");
+	//  	return;
+	//  }
 	draw();
 	update(); 	
 }
