5bf7edf3af2c3ced1fbf58ca036f2fb726cd8bc1
hp <chhabraashish123@gmail.com>
2020-06-14 22:08:51 +0530

    Left And Right Boundary fixed

--- snake.js
+++ snake.js
@@ -5,6 +5,7 @@ function init()
    W=canvas.width=600;
    H=canvas.height=600;
    pen=canvas.getContext('2d');
+   game_over=false;
    rect={
    	x:20,
    	y:20,
@@ -18,19 +19,23 @@ function init()
 function draw()
 {
    pen.clearRect(0,0,W,H);
-   pen.fillStyle="red";
    pen.fillRect(rect.x,rect.y,rect.w,rect.h);
+   pen.fillStyle="red";
 }
 function update()
 { 
    rect.x+=rect.speed;
+   if(rect.x>W-rect.w||rect.x<0)
+   	  rect.speed *= -1;
 }
 function gameloop()
 {
-	// if(game_over==true)
-	// 	clearInterval(f);
-	//  	alert("Game Over");
-	//  	return;
+	if(game_over==true)
+	{
+		clearInterval(f);
+	 	alert("Game Over");
+	 	return;
+	 }
 	draw();
 	update(); 	
 }
